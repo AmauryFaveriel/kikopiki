fde32f25eb005f5db8d572fb913a08682ffbc373
AmauryFaveriel <mrfvrl@gmail.com>
2018-12-12 16:33:48 +0100

    :tada: Change my name in the url

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e18fd81edf261f8897a1b4afce903af5.js"
+  "/kikopiki/precache-manifest.fd68a7defc9e40e2ce307818225c6550.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/kikopiki/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
