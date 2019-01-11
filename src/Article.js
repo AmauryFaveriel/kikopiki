4b6eb0d204ddc13436199abf548b97c009fbcf96
blyndusk <alexandredelaloydev@gmail.com>
2019-01-11 11:43:52 +0100

    :boom: now others paintings change with scroll on article

--- src/Article.js
+++ src/Article.js
@@ -1,33 +1,39 @@
 import React, { Component } from 'react';
 import data from './data';
 
-class TwoPaitings extends Component {
-    render = () => {
-        return (
-            // <section className="Article__imgBox">
-            //     <img src={data.paintings[this.props.index].first_painting} alt="" className="Article__imgBox__img"/>
-            //     <img src={data.paintings[this.props.index].second_painting} alt="" className="Article__imgBox__img"/>
-            // </section>
-            <section className="Article__imgBoxes">
-                <div className='Article__imgBoxes__imgBox Article__imgBoxes__imgBox__resizable'>
-                    <div className='Article__imgBoxes__imgBox__resizable__resizer'></div>
-                    <img className='Article__imgBoxes__imgBox__img' src="https://images.unsplash.com/photo-1544027549-4f862509c707?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=670&q=80" alt=""/>
-                </div>
-                <div className='Article__imgBoxes__imgBox'>
-                    <img className='Article__imgBoxes__imgBox__img' src="https://images.unsplash.com/photo-1540483761890-a1f7be05d99f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=690&q=80" alt=""/>
-                </div>
-            </section>
-        )
-    }
-}
+import TwoPaitings from './TwoPaintings';
 class Article extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            otherIndex: 0
+        }
+    }
+    logScroll = () =>{
+        let contentScrollTop = document.getElementById('wow').scrollTop;
+        if (contentScrollTop <= 200) {
+            this.setState({otherIndex: 0})
+        }
+        else if (contentScrollTop <= 400) {
+            this.setState({otherIndex: 1})
+        }
+        else if (contentScrollTop <= 600) {
+            this.setState({otherIndex: 2})
+        }
+        else  {
+            this.setState({otherIndex: 3})
+        }
+        return console.log(contentScrollTop)
+    }
     render = () => {
         return (
             <article className={this.props.style}>
                 <TwoPaitings
                     index={this.props.index}
+                    otherIndex={this.state.otherIndex}
                 />
-                <article className="Article__content">
+                <article id="wow" className="Article__content" 
+                onScroll={this.logScroll}>
                     <h2 className="Article__content__title">{data.paintings[this.props.index].title}</h2>
                     <h3 className="Article__content__subtitle">{data.paintings[this.props.index].desc}</h3>
                     <p className="Article__content__text">{data.lorem_1}</p>
--- src/TwoPaintings.js
+++ src/TwoPaintings.js
@@ -7,19 +7,14 @@ class TwoPaitings extends Component {
             <section className="Article__imgBoxes">
                 <div className='Article__imgBoxes__imgBox Article__imgBoxes__imgBox__resizable'>
                     <div className='Article__imgBoxes__imgBox__resizable__resizer'></div>
-                    <img className='Article__imgBoxes__imgBox__img' src={data.paintings[this.props.index].first_painting} alt=""/>
+                    <img className='Article__imgBoxes__imgBox__img' src={data.paintings[this.props.index].main_painting} alt=""/>
                 </div>
                 <div className='Article__imgBoxes__imgBox'>
-                    <img className='Article__imgBoxes__imgBox__img' src={data.paintings[this.props.index].second_painting} alt=""/>
+                    <img className='Article__imgBoxes__imgBox__img' src={data.paintings[this.props.index].other_paintings[this.props.otherIndex]} alt=""/>
                 </div>
             </section>
         )
     }
 }
-
-// <section className="Article__imgBox">
-            //     <img src={data.paintings[this.props.index].first_painting} alt="" className="Article__imgBox__img"/>
-            //     <img src={data.paintings[this.props.index].second_painting} alt="" className="Article__imgBox__img"/>
-            // </section>
 
 export default TwoPaitings;
