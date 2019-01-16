196b2868156b99f0c6de3d5c4d0f4ffe00705307
blyndusk <alexandredelaloydev@gmail.com>
2019-01-16 22:32:45 +0100

    🚧 WIP - zooms in

--- src/TwoPaintings.js
+++ src/TwoPaintings.js
@@ -7,10 +7,18 @@ class TwoPaitings extends Component {
             <section className="Article__imgBoxes">
                 <div className='Article__imgBoxes__imgBox Article__imgBoxes__imgBox__resizable'>
                     <div className={this.props.style}></div>
-                    <img className='Article__imgBoxes__imgBox__img Article__imgBoxes__imgBox__img--showed' src={data.paintings[this.props.index].main_painting.visual} alt=""/>
+                    <img 
+                        className='Article__imgBoxes__imgBox__img' 
+                        src={this.props.painting.inspired_painting.visual} 
+                        style={this.props.inspiredStyle}
+                        alt=""/>
                 </div>
                 <div className='Article__imgBoxes__imgBox'>
-                    <img className='Article__imgBoxes__imgBox__img' src={data.paintings[this.props.index].inspired_painting.visual} alt=""/>
+                    <img 
+                    className='Article__imgBoxes__imgBox__img'
+                    src={this.props.painting.main_painting.visual}
+                    style={this.props.mainStyle}
+                    alt=""/>
                 </div>
             </section>
         )
