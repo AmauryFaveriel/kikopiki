57564090f0977a2d36bcb9ed9b7a22c29feb1c2c
blyndusk <alexandredelaloydev@gmail.com>
2019-01-17 11:59:19 +0100

    🐛 fix conflicts

--- src/ArticleHeader.js
+++ src/ArticleHeader.js
@@ -2,18 +2,7 @@ import React, { Component } from 'react';
 import data from './data';
 import ArticleHeaderItem from './ArticleHeaderItem';
 
-<<<<<<< HEAD
 class ArticleHeader extends Component { 
-=======
-class ArticleHeader extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {painting: data.paintings[this.props.index]}
-    }
-    UNSAFE_componentWillUpdate = (nextProps) => {
-        if (nextProps.index !== this.props.index) this.setState({painting: data.paintings[nextProps.index]})
-    }
->>>>>>> 9e09dcc64763c135df5439107ab5a73509ebd556
     render = () => {
         return (
             <header className="ArticleHeader">
