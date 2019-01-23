d4d5141d4fe1fa37c2e3acb4d51fa0d5e4964087
blyndusk <alexandredelaloydev@gmail.com>
2019-01-23 16:14:04 +0100

    :recycle: Menu component clean code

--- src/Components/Menu/LittlePaintings/LittlePainting/LittlePainting.js
+++ src/Components/Menu/LittlePaintings/LittlePainting/LittlePainting.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
 
-import checked from "../../../assets/imgs/checked.svg"
-import unchecked from "../../../assets/imgs/unchecked.svg"
+import checked from "../../../../assets/imgs/checked.svg"
+import unchecked from "../../../../assets/imgs/unchecked.svg"
 class LittlePaiting extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            style: 'LittlePainting',
+            hiddenStyle: 'LittlePainting hidden'
+        }
+    }
     render() {
         return (
-            <li className={this.props.style} 
-                onClick={() => this.props.updateArticle(this.props.index)}>
+            <li className={!this.props.isSameIndex ? this.state.style : this.state.hiddenStyle} 
+                onClick={() => this.props.updateArticle(this.props.i)}>
                 <img src={this.props.painting} alt={this.props.title}/>
-                <img className="Painting__checked" alt="checked icon" src={this.props.checkedPaintings.includes(this.props.index) ? checked : unchecked}/>
+                <img className="Painting__checked" alt="checked icon" src={this.props.visitedPaintings.includes(this.props.i) ? checked : unchecked}/>
             </li>
         )
     }
--- src/Components/Menu/Menu.js
+++ src/Components/Menu/Menu.js
@@ -1,45 +1,27 @@
 import React, { Component } from 'react';
-import LittlePaiting from './LittlePainting/LittlePainting';
 import logo from '../../assets/imgs/logo.svg'
-import data from '../../data';
+import LittlePaintings from './LittlePaintings/LittlePaintings';
 
 class Menu extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            isMenuHidden: false,
-            navPaintingsStyle: 'Menu__nav__paintings',
-            navPaintingsStyleHidden: 'Menu__nav__paintings Menu__nav__paintings--hidden',
-            navConclusionStyle: 'Menu__nav__conclusion',
-            navConclusionStyleHidden: 'Menu__nav__conclusion hidden',
+            isMenuHidden: false
         }
     }
-    hideMenu = () => this.setState({isMenuHidden: this.state.isMenuHidden ? false : true})
-    toggleSound = () => this.setState({isSoundOn: this.state.isSoundOn ? false : true})
-    
+    hideMenu = () => this.setState({isMenuHidden: !this.state.isMenuHidden})
     render = () => {
         return (
             <section className={this.props.style}>
-                <img src={logo} alt="" className="Menu__logo" onClick={this.hideMenu}/>
+                <img src={logo} alt="logo" className="Menu__logo" onClick={this.hideMenu}/>
                 <nav className="Menu__nav">
-                    <ul className={this.state.isMenuHidden ? this.state.navPaintingsStyleHidden : this.state.navPaintingsStyle}>
-                    {data.paintings.map((x, i) =>
-                        <LittlePaiting
-                            style={this.props.theIndex === i ? 'Menu__nav__painting hidden' : 'Menu__nav__painting'}
-                            key={i}
-                            index={i}
-                            painting={x.main_painting.visual}
-                            title={x.title}
-                            updateArticle={() => this.props.updateArticle(i)}
-                            checkedPaintings={this.props.checkedPaintings}
-
-                        />
-                    )}
-                    </ul>
-                    <button 
-                        className={this.state.isMenuHidden ? this.state.navConclusionStyleHidden : this.state.navConclusionStyle}
-                        onClick={this.props.click}
-                    >GO TO CONCLUSION</button>
+                    <LittlePaintings
+                        AppIndex={this.props.AppIndex}
+                        visitedPaintings={this.props.visitedPaintings}
+                        updateArticle={(i) => this.props.updateArticle(i)}
+                        click={this.props.click}
+                        isMenuHidden={this.state.isMenuHidden}
+                    />
                 </nav>
             </section>
         )
