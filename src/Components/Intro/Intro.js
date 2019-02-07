f397d54b05e587957e383a312e1e678688266b38
blyndusk <alexandredelaloydev@gmail.com>
2019-02-07 16:47:52 +0100

    ♻️ clean Intro code

--- src/Components/Intro/Intro.js
+++ src/Components/Intro/Intro.js
@@ -1,23 +1,18 @@
 import React, { Component } from "react";
-
 import data from "../../data";
 
 class Intro extends Component {
-  render = () => {
-    return (
-      <section className={this.props.style}>
-        <h1 className="Intro__title">{data.intro.title}</h1>
-        <div className="Intro__descContainer">
-          <p className="Intro__desc">{data.intro.intro_1}</p>
-          <p className="Intro__desc">{data.intro.intro_2}</p>
-        </div>
-        <button className="Intro__cta" onClick={this.props.hideIntro}>
-          {data.intro.cta}
-        </button>
-        <section className={this.props.overlayStyle}/>
-      </section>
-    );
-  };
+    render = () => {
+        return <section className={this.props.bool ? 'Intro Intro--hidden' : 'Intro'}>
+            <h1 className="Intro__title">{data.intro.title}</h1>
+            <div className="Intro__descContainer">
+            <p className="Intro__desc">{data.intro.intro_1}</p>
+            <p className="Intro__desc">{data.intro.intro_2}</p>
+            </div>
+            <button className="Intro__cta" onClick={this.props.click}>{data.intro.cta}</button>
+            <section className={this.props.overlayStyle}/>
+        </section>
+    };
 }
 
 export default Intro;
